2dd26c34d401879b231a0a86deec730a1032d145
Istvan <nomak8702@gmail.com>
2020-12-13 10:58:45 +0200

    update 2 homework curs18

--- src/main/java/ro/fasttrackit/homework/curs18/homework/webcountries/CountryReader.java
+++ src/main/java/ro/fasttrackit/homework/curs18/homework/webcountries/CountryReader.java
@@ -1,38 +1,28 @@
 package ro.fasttrackit.homework.curs18.homework.webcountries;
 
+import org.springframework.stereotype.Component;
+
 import java.io.BufferedReader;
 
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
+@Component
 public class CountryReader {
+    private static   int idNumber;
 
-    public static List<Country> rCountry(String line) {
+    public static List<Country> rCountry(String text) {
+
         List<Country> result = new ArrayList<>();
-        int idNumber = -1;
-        String readL;
-        Country country;
-
+        String line;
         try {
-            BufferedReader read = new BufferedReader(new FileReader(line));
-            while ((readL = read.readLine()) != null) {
-                List<String> listNeighbours = new ArrayList<>();
-                String[] lineCountry = readL.split("[|~]");
-                for (String conty : lineCountry) {
-                    if ((conty == conty.toUpperCase()) && (!conty.matches(".*\\d.*"))) {
-                        listNeighbours.add(conty);
-                    }
-                }
-                country = new Country(++idNumber,
-                        lineCountry[0],
-                        lineCountry[1],
-                        Long.parseLong(lineCountry[2]),
-                        Long.parseLong(lineCountry[3]),
-                        lineCountry[4],
-                        listNeighbours.equals(new ArrayList<>()) ? new ArrayList<>() : listNeighbours);
-                result.add(country);
+            BufferedReader read = new BufferedReader(new FileReader(text));
+            while ((line = read.readLine()) != null) {
+                idNumber++;
+                result.add(getCountry(line));
             }
 
         } catch (IOException e) {
@@ -41,4 +31,31 @@ public class CountryReader {
 
         return result;
     }
+
+    private static Country getCountry(String line){
+
+      String[] lineCountry=line.split("[|]");
+      Country country;
+        List<String> neighbours=new ArrayList<>();
+        if(lineCountry.length>5){
+            neighbours= listNeighbours(lineCountry[5]);
+        }
+        country=new Country(idNumber, lineCountry[0],
+              lineCountry[1],
+              Long.parseLong(lineCountry[2]),
+              Long.parseLong(lineCountry[3]),
+              lineCountry[4],neighbours);
+      
+        return country;
+    }
+
+    private static List<String> listNeighbours(String neighbours) {
+        String[] neighboursList=neighbours.split("[~]");
+        List<String> result=new ArrayList<>();
+        for(String neig:neighboursList){
+            result.add(neig);
+        }
+        return result;
+    }
+
 }
